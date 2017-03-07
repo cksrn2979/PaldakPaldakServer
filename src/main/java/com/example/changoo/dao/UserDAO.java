fc63f4e58c119944c509d4fa625190126c58c358
changoo <changoo@Changoo-PC>
2017-03-07 19:02:29 +0900

    EE

---
+++ src/main/java/com/example/changoo/dao/UserDAO.java
@@ -0,0 +1,18 @@
+package com.example.changoo.dao;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAO {
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+}
---
+++ src/main/java/com/example/changoo/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.changoo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.changoo.dao.UserDAO;
+
+@Service
+public class UserService {
+	private UserDAO userDAO;
+}
