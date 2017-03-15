0e945eed472c259bfbbaa8eb86682ffd523e8ea5
songi3 <wks4j1004@gmail.com>
2017-03-15 23:51:47 +0900

    가입 페이지 생성 / id,pw,name 서버에서 받기

--- src/main/java/com/example/changoo/httpConnect/Protocol.java
+++ src/main/java/com/example/changoo/httpConnect/Protocol.java
@@ -8,4 +8,6 @@ public class Protocol {
     public static final String CHECKING_USER="CHECKING_USER";
     public static final String USER_OK="USER_OK";
     public static final String USER_NOK="USER_NOK";
+    public static final String JOIN_OK="JOIN_OK";
+    public static final String JOIN_NOK="JOIN_NOK";
 }
